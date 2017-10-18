e5abc716c44f180d88c3c0f5a215ed593560f9a0
Ian Rose <ian.r.rose@gmail.com>
2017-10-18 11:20:26 -0700

    Clean up proxy.

--- jupyterlab_github/__init__.py
+++ jupyterlab_github/__init__.py
@@ -1,25 +1,47 @@
-from notebook.utils import url_path_join
-from notebook.base.handlers import APIHandler
-from tornado.httpclient import HTTPClient, HTTPRequest, HTTPError
 import tornado.web as web
 from tornado.httputil import url_concat
+from tornado.httpclient import HTTPClient, HTTPRequest, HTTPError
+
 from traitlets import Unicode
 from traitlets.config import Configurable
+
+from notebook.utils import url_path_join
+from notebook.base.handlers import APIHandler
 
 path_regex = r'(?P<path>(?:(?:/[^/]+)+|/?))'
 GITHUB_API = 'https://api.github.com'
 
 class GitHubConfig(Configurable):
+    """
+    A Configurable that declares the 'client_id' and 'client_secret'
+    parameters.
+    """
     client_id = Unicode('', config=True,
         help='The Client ID for the GitHub OAuth app')
     client_secret = Unicode('', config=True,
         help='The Client secret for the GitHub OAuth app')
 
 class GitHubHandler(APIHandler):
+    """
+    A proxy for the GitHub API v3.
+
+    The purpose of this proxy is to add the 'client_id' and 'client_secret'
+    tokens to the API request, which allows for a higher rate limit.
+    Without this, the rate limit on unauthenticated calls is so limited as
+    to be practically useless.
+    """
     def get(self, path = ''):
+        """
+        Proxy API requests to GitHub, adding 'client_id' and 'client_secret'
+        if they have been set.
+        """
+
+        # Get access to the notebook config object
         c = GitHubConfig(config=self.config)
         try:
             api_path = url_path_join(GITHUB_API, path)
+            # If the config has client_id and client_secret set,
+            # apply them to the request.
             if c.client_id != '' and c.client_secret != '':
                 api_path = url_concat(api_path,
                     {'client_id': c.client_id, 'client_secret': c.client_secret})
