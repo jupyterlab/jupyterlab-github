22d487a53b176967b854b0b28f44a1f500953d43
Ian Rose <ian.r.rose@gmail.com>
2017-10-20 19:31:39 -0700

    Publish v0.1.0

--- setup.py
+++ setup.py
@@ -5,8 +5,24 @@ import setuptools
 
 setuptools.setup(
     name='jupyterlab_github',
+    description='A Jupyter Notebook server extension which acts as a proxy for GitHub API requests.',
     version='0.1.0',
     packages=setuptools.find_packages(),
+    author          = 'Jupyter Development Team',
+    author_email    = 'jupyter@googlegroups.com',
+    url             = 'http://jupyter.org',
+    license         = 'BSD',
+    platforms       = "Linux, Mac OS X, Windows",
+    keywords        = ['Jupyter', 'JupyterLab', 'GitHub'],
+    classifiers     = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
     install_requires=[
         'notebook'
     ],
